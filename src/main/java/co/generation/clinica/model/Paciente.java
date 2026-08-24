c26e9b51bd580a2df6da80d163c5326f24064fe9
unknown <vannessamatallana97@gmail.com>
2026-08-24 12:54:54 -0500

    Implementados metodos, validaciones e interfaz Registrable en Paciente

--- src/main/java/co/generation/clinica/model/Paciente.java
+++ src/main/java/co/generation/clinica/model/Paciente.java
@@ -1,6 +1,30 @@
 package co.generation.clinica.model;
 
-public class Paciente {
+import co.generation.clinica.interfaces.Registrable;
+import java.util.Objects;
+
+public class Paciente implements Registrable {
+    private int id;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Paciente(int id, String cedula, String nombre, String apellido, String telefono) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public Paciente(String cedula, String nombre, String apellido, String telefono) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
     public int getId() {
         return id;
     }
@@ -20,6 +44,7 @@ public class Paciente {
     public String getTelefono() {
         return telefono;
     }
+
 
     public void setId(int id) {
         this.id = id;
@@ -49,41 +74,52 @@ public class Paciente {
     public void setTelefono(String telefono) {
         if (telefono.trim() == "") {
             throw new IllegalArgumentException("Telefono no puede estar vacío!");
-        } else if (telefono.matches("^[0-9]{7,10}$")){
+        } else if (telefono.matches("^[0-9]{7,10}$")) {
             throw new IllegalArgumentException("Telefono invalido!");
 
         }
         this.telefono = telefono;
     }
 
-    private int id;
-    private String cedula;
-    private String nombre;
-    private String apellido;
-    private String telefono;
 
-    public Paciente(int id, String cedula, String nombre, String apellido, String telefono) {
-        this.id = id;
-        this.cedula = cedula;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
+
+    @Override
+    public boolean esValido() {
+        try {
+            return cedula != null && !cedula.trim().isEmpty() &&
+                    nombre != null && !nombre.trim().isEmpty() &&
+                    apellido != null && !apellido.trim().isEmpty() &&
+                    telefono != null && !telefono.matches("^[0-9]{7,10}$");
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    public Paciente(String cedula, String nombre, String apellido, String telefono) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
+    @Override
+    public String getDatosRegistro() {
+        return toString();
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Paciente otro){
-            if(this.cedula == otro.cedula){
-                return  true;
+        if (obj instanceof Paciente otro) {
+            if (this.cedula == otro.cedula) {
+                return true;
             }
         }
-            return false;
+        return false;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cedula);
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " " + cedula + " " + telefono;
+    }
+
 }
+
+
